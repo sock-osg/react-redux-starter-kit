6b702606c56cdb34cc00e1a709516393331d58df
Samuel Quintana <sock.sqt@gmail.com>
2017-11-13 20:32:17 -0600

    Integrated clicked menu

--- src/routes/Home/components/HomeView.js
+++ src/routes/Home/components/HomeView.js
@@ -2,10 +2,13 @@ import React from 'react'
 import DuckImage from '../assets/Duck.jpg'
 import './HomeView.scss'
 
+import Menu from 'components/commons/menu/menu'
+
 export const HomeView = () => (
   <div>
     <h4>Welcome!</h4>
     <img alt='This is a duck, because Redux!' className='duck' src={DuckImage} />
+    <Menu />
   </div>
 )
 
